9aba1531a7ddc303bb4b4c5e03b15906aed84dae
Kolten ✔️ <kolten.pilar@gmail.com>
2023-09-06 14:57:37 -0400

    tested server with Postman, GET request working

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ const port = process.env.PORT || 5000; //port
 
 const app = express(); 
 
+app.get('/api/users', (req, res) => {
+  res.json({message: 'Get Users'});
+})
+
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
