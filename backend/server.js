63ac3eb8396583159f6003bcf89c8306cfb3e61c
Kolten ✔️ <kolten.pilar@gmail.com>
2023-09-06 14:51:08 -0400

    setup server.js / tested functionality / working

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log('Hello World');
+//Imports
+const express = require('express'); //backend framework
+const dotenv = require('dotenv').config(); //environment variables
+const port = process.env.PORT || 5000; //port
+
+const app = express(); 
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
