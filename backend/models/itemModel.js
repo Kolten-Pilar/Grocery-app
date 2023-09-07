3caa312f546a8745a1fe08e2b5a9caac0c469cca
Kolten ✔️ <kolten.pilar@gmail.com>
2023-09-07 11:16:26 -0400

    protected itemRoutes, added reference to user id to itemModel

--- backend/models/itemModel.js
+++ backend/models/itemModel.js
@@ -2,6 +2,11 @@ const mongoose = require("mongoose");
 
 const itemSchema = mongoose.Schema(
   {
+    user: {
+      type: mongoose.Schema.Types.ObjectId, //user id
+      required: true,
+      ref: "User", //reference to User model
+    },
     text: {
       type: String,
       required: [true, "Please enter text"],
--- backend/routes/itemRoutes.js
+++ backend/routes/itemRoutes.js
@@ -2,10 +2,11 @@
 const express = require('express');
 const router = express.Router();
 const {getItems, createItem, updateItem, deleteItem} = require('../controllers/itemController')
+const { protect } = require('../middleware/authMiddleware'); //import protect middleware
 
 //Routes
-router.route('/').get(getItems).post(createItem);
-router.route('/:id').put(updateItem).delete(deleteItem);
+router.route('/').get(protect, getItems).post(protect, createItem);
+router.route('/:id').put(protect, updateItem).delete(protect, deleteItem);
 
 
 module.exports = router; //export router
