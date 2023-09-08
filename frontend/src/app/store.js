9d54b3f56593d07c3165bd5b52f95e3d366bae71
Kolten ✔️ <kolten.pilar@gmail.com>
2023-09-08 16:00:45 -0400

    added functionality for register page / updated main.jsx

---
+++ frontend/src/app/store.js
@@ -0,0 +1,9 @@
+//imports
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from '../features/auth/authSlice';
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
+})
