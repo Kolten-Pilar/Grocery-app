7ff4249783bbf9ce4969b22122cd1e9a83615eae
Kolten ✔️ <kolten.pilar@gmail.com>
2023-09-11 12:39:12 -0400

    added frontend login / logout functionality. adjusted some styling on the logout button and loading spinner

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { FaDoorClosed, FaUserSlash, FaUser, FaUserPlus } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { logout, reset } from "../features/auth/authSlice";
 
 
 function Header() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth); 
  
+  const onLogout = () => {
+    dispatch(logout());
+    dispatch(reset());
+    navigate("/");
+  }
 
   return (
     <header className="flex justify-between items-center pr-5 mb-14 border-2 border-black rounded-xl bg-blue-700 text-white font-semibold">
@@ -13,13 +23,13 @@ function Header() {
       </div>
       <ul className="flex items-center justify-between">
         {/* Using a ternary below when login functionality is added. Checks for user before showing logout button */}
-        {/* {user ? ( */}
-           <li className=" ml-5">
-             <button className=" text-white font-semibold flex items-center justify-center" >
+        {user ? (
+           <li className=" ml-5 py-4">
+             <button className=" text-white font-semibold flex items-center justify-center" onClick={onLogout} >
                <FaUserSlash /> Logout
              </button>
            </li>
-        {/* ) : ( */}
+         ) : ( 
           <>
             <li className=" ml-5 py-4">
               <Link to="/login" className="flex items-center">
@@ -32,7 +42,7 @@ function Header() {
               </Link>
             </li>
           </>
-        {/* )} */}
+         )} 
       </ul>
     </header>
   );
