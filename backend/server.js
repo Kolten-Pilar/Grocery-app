e0608d7aeff1338f4927a3a895c841e27c27e393
Kolten ✔️ <kolten.pilar@gmail.com>
2023-09-13 13:55:59 -0400

    Prepared for deploy

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,21 @@ app.use('/api/users', require('./routes/userRoutes')); //use userRoutes
 
 app.use('/api/items', require('./routes/itemRoutes')); //use itemRoutes
 
+// Serve frontend build files in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get('*', (req, res) => 
+    res.sendFile(path.resolve(__dirname, '../', 'frontend', 'dist', 'index.html')
+    )
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('Please set to production mode');
+  })
+}
+
 //Error Handler
 app.use(errorHandler);
 
